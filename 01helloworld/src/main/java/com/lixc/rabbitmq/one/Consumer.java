1b29c1a7bd5ca9fc0936d4c277c14f665b080523
VVLixc <vc572936348@yeah.net>
2023-08-20 17:31:28 +0800

    update RabbitMQ Hello World Pattern

---
+++ 01helloworld/src/main/java/com/lixc/rabbitmq/one/Consumer.java
@@ -0,0 +1,44 @@
+package com.lixc.rabbitmq.one;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * com.lixc.rabbitmq.one
+ *
+ * @author Lixc
+ * @version 1.0
+ * @since 2023-08-20 17:07
+ */
+public class Consumer {
+    public static final String QUEUE_NAME = "Hello World";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("192.168.222.132");
+        connectionFactory.setUsername("lixc");
+        connectionFactory.setPassword("001214");
+        Channel channel = connectionFactory.newConnection().createChannel();
+        System.out.println("等待接收消息....");
+
+        // 推送的消息如何进行消费的接口回调
+        DeliverCallback deliverCallback = (String consumerTag, Delivery message) -> {
+            String messageBody = new String(message.getBody());//消息分为消息头、消息体；这里获取消息体
+            System.out.println("消费者接受消息：" + messageBody);
+        };
+
+        // 取消消费的一个回调接口 如在消费的时候队列被删除掉了
+        CancelCallback cancelCallback = (consumerTag) -> System.out.println("消息消费被中断");
+
+        /**
+         * 消费者消费消息
+         * 1.队列名称
+         * 2.消费成功之后是否要自动应答 true 代表自动应答 false 手动应答
+         * 3.传递消息时的回调
+         * 4.消费者取消消费的回调
+         */
+        channel.basicConsume(QUEUE_NAME, false, deliverCallback, cancelCallback);
+    }
+}
---
+++ 01helloworld/src/main/java/com/lixc/rabbitmq/one/Producer.java
@@ -0,0 +1,47 @@
+package com.lixc.rabbitmq.one;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * com.lixc.rabbitmq.one
+ *
+ * @author Lixc
+ * @version 1.0
+ * @since 2023-08-20 16:44
+ */
+public class Producer {
+    public static final String QUEUE_NAME = "Hello World";
+
+    public static void main(String[] args) throws Exception {
+        // 创建一个连接工厂
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("192.168.222.132");
+        connectionFactory.setUsername("lixc");
+        connectionFactory.setPassword("001214");
+        Channel channel = connectionFactory.newConnection().createChannel();
+        /**
+         * 生成一个队列
+         * 1.队列名称
+         * 2.队列里面的消息是否持久化 默认消息存储在内存中
+         * 3.该队列是否只供一个消费者进行消费 是否进行共享 true 可以多个消费者消费
+         * 4.是否自动删除 最后一个消费者端开连接以后 该队列是否自动删除 true 自动删除
+         * 5.其他参数
+         */
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+        /**
+         * 发送一个消息
+         * 1.发送到那个交换机
+         * 2.路由的 key 是哪个
+         * 3.其他的参数信息
+         * 4.发送消息的消息体
+         */
+        String message = "Hello World";
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+        System.out.println("生产者消息发送完毕");
+    }
+}
