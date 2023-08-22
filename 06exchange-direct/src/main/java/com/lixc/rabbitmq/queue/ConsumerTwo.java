4b2b84d43de0069bf28da7b49a969ce7fc837f87
VVLixc <vc572936348@yeah.net>
2023-08-22 22:18:56 +0800

    Exchanges：Direct-Exchange Code-Demo

--- 06exchange-direct/src/main/java/com/lixc/rabbitmq/queue/ConsumerTwo.java
+++ 06exchange-direct/src/main/java/com/lixc/rabbitmq/queue/ConsumerTwo.java
@@ -19,13 +19,14 @@ public class ConsumerTwo {
     public static void main(String[] args) throws Exception {
         Channel channel = RabbitMQUtils.getChannel();
         channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
-        channel.queueDeclare("queueOne", false, false, false, null);
+        channel.queueDeclare("queueTwo", false, false, false, null);
 
         // 同一个队列和交换机  可以绑定多个不同的RoutingKey
-        channel.queueBind("queueOne", EXCHANGE_NAME, "error");
+        channel.queueBind("queueTwo", EXCHANGE_NAME, "error");
+        channel.queueBind("queueTwo", EXCHANGE_NAME, "warning");
 
         System.out.println("消费者Two等待接收消息（通知模式---direct类型直接交换机）......");
-        channel.basicConsume("queueOne", true,
+        channel.basicConsume("queueTwo", true,
                 (consumerTag, message) -> System.out.println("消费者Two接收消息：" + new String(message.getBody(), StandardCharsets.UTF_8)),
                 consumerTag -> System.out.println("消费者Two取消消费者接口回调逻辑"));
     }
--- 06exchange-direct/src/main/java/com/lixc/rabbitmq/queue/Producer.java
+++ 06exchange-direct/src/main/java/com/lixc/rabbitmq/queue/Producer.java
@@ -5,7 +5,9 @@ import com.rabbitmq.client.BuiltinExchangeType;
 import com.rabbitmq.client.Channel;
 
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.Scanner;
+import java.util.Set;
 
 /**
  * com.lixc.rabbitmq.queue
@@ -20,11 +22,15 @@ public class Producer {
     public static void main(String[] args) throws Exception {
         Channel channel = RabbitMQUtils.getChannel();
         channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
-        Scanner scanner = new Scanner(System.in);
-        while (scanner.hasNext()) {
-            String message = scanner.next();
+
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("info", "普通info消息");
+        hashMap.put("warning", "警告warning消息");
+        hashMap.put("error", "错误error消息");
+        for (String routingKey : hashMap.keySet()) {
+            String message = hashMap.get(routingKey);
             System.out.println("生产者声明direct类型直接交换机 " + EXCHANGE_NAME + " 发送消息：" + message);
-            channel.basicPublish(EXCHANGE_NAME, "error", null, message.getBytes(StandardCharsets.UTF_8));
+            channel.basicPublish(EXCHANGE_NAME, routingKey, null, message.getBytes(StandardCharsets.UTF_8));
         }
     }
 }
